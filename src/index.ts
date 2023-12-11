681b960c8a20cb2c84d83609d8c633c7bb1ccb12
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-12-11 10:47:34 -0300

    feat: add express initial serve

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Olá mundeefeos!");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`listening on port ${port}`));
